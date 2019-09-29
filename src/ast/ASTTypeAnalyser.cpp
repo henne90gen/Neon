9c76cba66b5a7488b391584ffbefa7427adf81ab
Henne <henne90gen@gmail.com>
2019-09-29 18:27:34 +0200

    Added skeleton for type analyser

---
+++ src/ast/ASTTypeAnalyser.cpp
@@ -0,0 +1,14 @@
+#include "ASTTypeAnalyser.h"
+
+void ASTTypeAnalyser::visitFunctionNode(FunctionNode *node) {}
+void ASTTypeAnalyser::visitCallNode(CallNode *node) {}
+void ASTTypeAnalyser::visitVariableNode(VariableNode *node) {}
+void ASTTypeAnalyser::visitVariableDefinitionNode(VariableDefinitionNode *node) {}
+void ASTTypeAnalyser::visitBinaryOperationNode(BinaryOperationNode *node) {}
+void ASTTypeAnalyser::visitUnaryOperationNode(UnaryOperationNode *node) {}
+void ASTTypeAnalyser::visitAssignmentNode(AssignmentNode *node) {}
+void ASTTypeAnalyser::visitSequenceNode(SequenceNode *node) {}
+void ASTTypeAnalyser::visitStatementNode(StatementNode *node) {}
+void ASTTypeAnalyser::visitFloatNode(FloatNode *node) {}
+void ASTTypeAnalyser::visitIntegerNode(IntegerNode *node) {}
+void ASTTypeAnalyser::visitBoolNode(BoolNode *node) {}
---
+++ src/ast/ASTTypeAnalyser.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "AST.h"
+
+#include <unordered_map>
+
+class ASTTypeAnalyser : public ASTVisitor {
+  public:
+    void visitFunctionNode(FunctionNode *node) override;
+    void visitCallNode(CallNode *node) override;
+    void visitVariableNode(VariableNode *node) override;
+    void visitVariableDefinitionNode(VariableDefinitionNode *node) override;
+    void visitBinaryOperationNode(BinaryOperationNode *node) override;
+    void visitUnaryOperationNode(UnaryOperationNode *node) override;
+    void visitAssignmentNode(AssignmentNode *node) override;
+    void visitSequenceNode(SequenceNode *node) override;
+    void visitStatementNode(StatementNode *node) override;
+    void visitFloatNode(FloatNode *node) override;
+    void visitIntegerNode(IntegerNode *node) override;
+    void visitBoolNode(BoolNode *node) override;
+
+  private:
+    std::unordered_map<AstNode *, AstNode::DataType> typeMap = {};
+};
