96325e23edd077711b171d3f0f9f109831669da2
Hendrik Müller <henne90gen@gmail.com>
2021-03-12 13:22:59 +0100

    Added comments to Linker

--- src/main/Linker.cpp
+++ src/main/Linker.cpp
@@ -7,15 +7,31 @@ bool Linker::link() {
     //  -pie --eh-frame-hdr -m elf_x86_64
 
     std::string s = "ld";
+
+    // specify which dynamic linker should be used at runtime for dynamically linked libraries
     s += " -dynamic-linker /lib64/ld-linux-x86-64.so.2";
+
+    // specify output file name
     s += " -o " + buildEnv->buildDirectory + program->name;
+
+    // additional object files that have to be linked (they contain the actual entry point and some setup stuff)
     s += " /lib64/crt1.o";
     s += " /lib64/crti.o";
     s += " /lib64/crtn.o";
+
+    // search directory for c standard library and math library
     s += " -L/usr/lib/gcc/x86_64-pc-linux-gnu/9.2.0/";
-    s += " -L.";
+
+    // search directory for Neon standard library
+    s += " -L" + buildEnv->buildDirectory;
+
+    // specify the object file to link
     s += " " + buildEnv->buildDirectory + program->objectFileName;
-    s += " -lc -lm"; // TODO there used to be "-lgcc -lgcc_s" linked as well, but that doesn't work any more
+
+    // c: c standard library, m: math library
+    s += " -lc -lm";
+
+    // Neon standard library
     s += " -lNeonStd";
 
     std::cout << std::endl << "Calling linker with the following command:" << std::endl << s << std::endl;
